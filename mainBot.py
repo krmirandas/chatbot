123603ed08c91da9b1d7b835df0c8f38feffb04e
Kevin Miranda <kevinmiranda29@ciencias.unam.mx>
2021-01-17 23:00:24 -0600

    Se agrega a discord

--- mainBot.py
+++ mainBot.py
@@ -1,20 +1,23 @@
+import pickle  # guardar modelos
+import random  # escoger respeustas aleatorias
+import json
+import tensorflow
+import tflearn
+import numpy
 import nltk
-from nltk.stem.lancaster import LancasterStemmer #Mas entendible
+import discord
+from nltk.stem.lancaster import LancasterStemmer  # Mas entendible
 from tensorflow.python.compiler.tensorrt import trt_convert as trt
 stemmer = LancasterStemmer()
-import numpy #
-import tflearn #
-import tensorflow #
-import json #
-import random #escoger respeustas aleatorias
-import pickle #guardar modelos
 
-#nltk.download('punkt')
+# nltk.download('punkt')
 
-with open("contenido.json", encoding='utf-8') as archivo: 
+key = ''
+
+with open("contenido.json", encoding='utf-8') as archivo:
     datos = json.load(archivo)
 
-try: 
+try:
     with open("variables.pickle", "rb") as archivoPickle:
         palabras, tags, entrenamiento, salida = pickle.load(archivoPickle)
 except:
@@ -23,17 +26,18 @@ except:
     auxX = []
     auxY = []
 
-    for contenido in datos["contenido"]: 
+    for contenido in datos["contenido"]:
         for patrones in contenido["patrones"]:
             '''Diferencia entre tokenize y split 
             tokenize va a reconocer los puntos especiales automaticamente.
             En split se lo tienes que pasar
             '''
-            auxPalabra = nltk.word_tokenize(patrones) #Una frase, la separa en palabras.
+            auxPalabra = nltk.word_tokenize(
+                patrones)  # Una frase, la separa en palabras.
             palabras.extend(auxPalabra)
             auxX.append(auxPalabra)
             auxY.append(contenido["tag"])
-            
+
             if contenido["tag"] not in tags:
                 tags.append(contenido["tag"])
 
@@ -45,17 +49,17 @@ except:
 
     salidaVacia = [0 for _ in range(len(tags))]
 
-    for x,documento in enumerate(auxX): 
+    for x, documento in enumerate(auxX):
         cubeta = []
         auxPalabra = [stemmer.stem(w.lower()) for w in documento]
-        for w in palabras: 
+        for w in palabras:
             if w in auxPalabra:
                 cubeta.append(1)
-            else: 
+            else:
                 cubeta.append(0)
         filaSalida = salidaVacia[:]
-        #El la lista de los indices del tag
-        #El contendio de Y en cada uno de los indices
+        # El la lista de los indices del tag
+        # El contendio de Y en cada uno de los indices
 
         filaSalida[tags.index(auxY[x])] = 1
         entrenamiento.append(cubeta)
@@ -73,36 +77,40 @@ red = tflearn.input_data(shape=[None, len(entrenamiento[0])])
 red = tflearn.fully_connected(red, 100)
 red = tflearn.fully_connected(red, 100)
 red = tflearn.fully_connected(red, len(salida[0]), activation="softmax")
-red = tflearn.regression(red)  
+red = tflearn.regression(red)
 
 modelo = tflearn.DNN(red)
-try: 
+try:
     modelo.load("modelo.tflearn")
 except:
-    modelo.fit(entrenamiento, salida, n_epoch=1000, batch_size=100, show_metric=True)
+    modelo.fit(entrenamiento, salida, n_epoch=1000, batch_size=10, show_metric=True)
     modelo.save("modelo.tflearn")
 
+
 def mainBot():
+    global key
+    client = discord.Client()
     while True:
-        entrada = input("Tú: ")
-        cubeta = [0 for _ in range(len(palabras))]
-        entradaProcesada = nltk.word_tokenize(entrada)
-        entradaProcesada = [stemmer.stem(palabra.lower()) for palabra in entradaProcesada]
-        for palabraIndividual in entradaProcesada:
-            for i, palabra in enumerate(palabras):
-                if palabra == palabraIndividual:
-                    cubeta[i] = 1
+        @client.event
+        async def on_message(message):
+            if message.author == client.user:
+                return
+            cubeta = [0 for _ in range(len(palabras))]
+            entradaProcesada = nltk.word_tokenize(message.content)
+            entradaProcesada = [stemmer.stem(
+                palabra.lower()) for palabra in entradaProcesada]
+            for palabraIndividual in entradaProcesada:
+                for i, palabra in enumerate(palabras):
+                    if palabra == palabraIndividual:
+                        cubeta[i] = 1
 
-        resultados = modelo.predict([numpy.array(cubeta)])
-        resultadosIndices = numpy.argmax(resultados)
-        tag = tags[resultadosIndices]
+            resultados = modelo.predict([numpy.array(cubeta)])
+            resultadosIndices = numpy.argmax(resultados)
+            tag = tags[resultadosIndices]
 
-        for tagAux in datos["contenido"]:
-            if tagAux["tag"] == tag:
-                respuesta = tagAux["respuestas"]
-        print("BOT:", random.choice(respuesta))
-
-
-        print(resultados)
-        print(resultadosIndices)
+            for tagAux in datos["contenido"]:
+                if tagAux["tag"] == tag:
+                    respuesta = tagAux["respuestas"]
+            await message.channel.send(random.choice(respuesta))
+        client.run(key)
 mainBot()
